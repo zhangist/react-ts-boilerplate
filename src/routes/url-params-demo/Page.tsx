e9f9427fd9cf4e062df87d9e082e6bae1c90f7ac
zhangist <zhangist@163.com>
2019-04-10 18:17:00 +0800

    fix url-params-demo bug

--- src/routes/url-params-demo/Page.tsx
+++ src/routes/url-params-demo/Page.tsx
@@ -1,7 +1,8 @@
 import * as React from "react";
-import { Link, RouteComponentProps, withRouter } from "react-router-dom";
+import { Link, Switch, Route } from "react-router-dom";
+import DefaultLoader from "./routes/default/Loader";
 
-export interface PageProps extends RouteComponentProps<{ param?: string }> {}
+export interface PageProps {}
 export interface PageState {}
 
 class Page extends React.Component<PageProps, PageState> {
@@ -10,16 +11,19 @@ class Page extends React.Component<PageProps, PageState> {
       <div>
         <div style={{ padding: "10px 8px" }}>Url Params Demo</div>
         <div style={{ padding: "10px 8px" }}>
+          <Link to="/url-params-demo">default</Link>
+          &nbsp;
           <Link to="/url-params-demo/1">param-1</Link>
-          <br />
+          &nbsp;
           <Link to="/url-params-demo/2">param-2</Link>
         </div>
-        <div style={{ padding: "10px 8px" }}>
-          <div>current param: {this.props.match.params.param}</div>
-        </div>
+        <Switch>
+          <Route path="/url-params-demo/:param" exact={true} component={DefaultLoader} />
+          <Route path="/url-params-demo" exact={true} component={DefaultLoader} />
+        </Switch>
       </div>
     );
   }
 }
 
-export default withRouter(Page);
+export default Page;
