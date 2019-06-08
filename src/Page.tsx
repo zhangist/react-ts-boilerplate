f1072727487e43e5934e93be339758619f00d644
yongbin zhang <yongbinzhang@yongbindeMacBook-Pro.local>
2019-06-08 11:14:56 +0800

    add errorboundary

--- src/Page.tsx
+++ src/Page.tsx
@@ -8,6 +8,7 @@ import { HistoryService } from "./services/historyService";
 import { I18nService } from "./services/i18nService";
 import { StoreService } from "./services/storeService";
 import { reducer } from "./store/reducer";
+import ErrorBoundary from "./components/ErrorBoundary";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
 import Loading from "./components/Loading";
@@ -49,36 +50,38 @@ class Page extends React.Component<PageProps, PageState> {
     }
 
     return (
-      <I18nextProvider i18n={i18n}>
-        <Provider store={store}>
-          <Router history={history}>
-            <div className="app">
-              <div className="header">
-                <Header />
+      <ErrorBoundary>
+        <I18nextProvider i18n={i18n}>
+          <Provider store={store}>
+            <Router history={history}>
+              <div className="app">
+                <div className="header">
+                  <Header />
+                </div>
+                <div className="content">
+                  <Switch>
+                    <Route path="/" exact={true} component={DefaultLoader} />
+                    <Route path="/redux-demo" component={ReduxDemoLoader} />
+                    <Route path="/http-demo" component={HttpDemoLoader} />
+                    <Route path="/pages-demo" component={PagesDemoLoader} />
+                    <Route
+                      path="/url-params-demo"
+                      component={UrlParamsDemoLoader}
+                    />
+                    <Route path="/i18n-demo" component={I18nDemoLoader} />
+                    <Route path="/theme-demo" component={ThemeDemoLoader} />
+                    <Route path="/user" component={UserLoader} />
+                    <Route component={NotFound} />
+                  </Switch>
+                </div>
+                <div className="footer">
+                  <Footer />
+                </div>
               </div>
-              <div className="content">
-                <Switch>
-                  <Route path="/" exact={true} component={DefaultLoader} />
-                  <Route path="/redux-demo" component={ReduxDemoLoader} />
-                  <Route path="/http-demo" component={HttpDemoLoader} />
-                  <Route path="/pages-demo" component={PagesDemoLoader} />
-                  <Route
-                    path="/url-params-demo"
-                    component={UrlParamsDemoLoader}
-                  />
-                  <Route path="/i18n-demo" component={I18nDemoLoader} />
-                  <Route path="/theme-demo" component={ThemeDemoLoader} />
-                  <Route path="/user" component={UserLoader} />
-                  <Route component={NotFound} />
-                </Switch>
-              </div>
-              <div className="footer">
-                <Footer />
-              </div>
-            </div>
-          </Router>
-        </Provider>
-      </I18nextProvider>
+            </Router>
+          </Provider>
+        </I18nextProvider>
+      </ErrorBoundary>
     );
   }
 }
